38251d894ae2b10125df7b6a6691735a0cfab1d6
saoufi sami <saoufi-sami@hotmail.fr>
2026-03-12 16:18:30 +0100

    init exo questionnaire

---
+++ Questionnaire v1/main.py
@@ -0,0 +1,15 @@
+# LES FONCTIONS : PROJET QUESTIONNAIRE
+#
+# Question : Quelle est la capitale de la france ?
+# (a) Marseille
+# (n) Nice
+# (c) Paris
+# (d) Nantes
+#
+# Votre réponse :
+# Bonne réponse / Mauvaise réponse
+
+# ...
+# Question : Quelle est la capitale de l'Italie ?
+#...
+#
