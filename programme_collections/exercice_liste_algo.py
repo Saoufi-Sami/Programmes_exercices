357e385f33cabb351fc73ff139a05a2188266907
saoufi sami <saoufi-sami@hotmail.fr>
2026-03-26 12:36:39 +0100

    exo 2 algo v1

--- programme_collections/exercice_liste_algo.py
+++ programme_collections/exercice_liste_algo.py
@@ -2,12 +2,20 @@
 
 # exemple d'application de taxi qui recherche le chauffeur le plus proche
 
+nom_chauffeur =         ["Patrick", "Paul", "Marc", "Jean", "Pierre", "Marie", "Maxime"]
+distance_chauffeur_km = [      1.5,    2.2,    0.4,    0.9,      7.1,     1.1,      0.6]
 
-distance_chauffeur_km = [1.5, 2.2, 0.4, 0.9, 7.1, 1.1, 0.6]
 
+index_min = 0
 distance_min = distance_chauffeur_km[0]
-for distance in distance_chauffeur_km:
+for i in range(len(distance_chauffeur_km)):
+    distance = distance_chauffeur_km[i]
     if distance < distance_min:
         distance_min = distance
+        index_min = i
 
-print("Distance minimal:",distance_min)
+
+
+print("Distance minimal:",distance_min, "Km")
+print("Index de la distance minimal : ", index_min)
+print("Nom du chauffeur :", nom_chauffeur[index_min])
