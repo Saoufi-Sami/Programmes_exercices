7149746044c729eb9a19cefb6b2c83ce9daba7d5
saoufi sami <saoufi-sami@hotmail.fr>
2026-03-27 09:47:44 +0100

    afficher premiere et derniere pizza + guard clause si la collection est vide

--- programme_collections/projet_pizzas_listes.py
+++ programme_collections/projet_pizzas_listes.py
@@ -1,10 +1,16 @@
 
 def afficher(collection):
+    if not collection :
+        print("AUCUNE PIZZA")
+        return
     print(f"---- LISTE DES PIZZAS ({len(collection)}) ----")
     for i in collection:
         print(i)
+    print()
+    print("Premiere pizza : " + collection[0])
+    print("Derniere pizza : " +collection[-1])
 
 
 pizzas = ("4 fromages", "végétarienne", "hawai", "calzone")
-
-afficher(pizzas)
+vide =()
+afficher(vide)
