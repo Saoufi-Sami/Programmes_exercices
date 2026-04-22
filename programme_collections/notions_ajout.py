4cee9fd4a489aabdca87e395efd94ec0f5658402
saoufi sami <saoufi-sami@hotmail.fr>
2026-04-22 15:51:23 +0200

    notions any isdigit etc

--- programme_collections/notions_ajout.py
+++ programme_collections/notions_ajout.py
@@ -34,3 +34,35 @@ print(slices_noms)
 
 # alors que "slices_noms = noms" ferait pointer vers la même liste
 # donc les deux print seraient identiques
+
+# Tris : Sort / Sorted
+
+#          0         1         2            3         4
+noms = ["Jean" , "Sophie" , "Martin", "Christophe", "Zoe"]
+
+noms.sort(reverse=True) #in place ici on tri sur la liste principal sort tri par ordre alphabetique et du plus petit au plus grand
+noms_tries = sorted(noms, reverse=True) # on cree une nouvelle liste
+
+print(noms)
+print(noms_tries)
+
+# operations sur les éléments : min,max, in, sum
+# Swapper deux éléments (échanger)
+noms[0], noms[4] = noms[4],noms[0]
+# join : rejoindre -> coller et split -> separer
+noms_join = ', '.join(noms)
+print(noms_join)
+a = "Paul-Marc-Emilie"
+a_split = a.split("-")
+print(a_split)
+# index, find et count
+# Les compréhension de listes
+# pour avoir par ex la longueur des caracteres de chaque mot on va faire simplement : longeur_noms = [len(nom) for nom in noms], en gros c'est une sintaxe bcp plus consise
+# print(longeur_noms)
+# Any et All n'importe quelle et tous
+nom_avec_un_z_existe = all([True if "z" in nom.lower() else False for nom in noms])
+if nom_avec_un_z_existe:
+    print("Trouvé")
+else:
+    print("non trouvé")
+# Any des qu'il trouve un true quelqupart il return true et isdigit il retourne true si il ya un chiffre
