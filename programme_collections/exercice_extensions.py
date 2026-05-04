eb1aade1d41e5ffd446652692870555c6df0a798
saoufi sami <saoufi-sami@hotmail.fr>
2026-05-04 10:43:26 +0200

    refactoring the code

--- programme_collections/exercice_extensions.py
+++ programme_collections/exercice_extensions.py
@@ -29,19 +29,19 @@ definition_extensions = (("doc", "Document Word"),
 
 
 
-for fichier in fichiers:
-    trouve = False
+'''for fichier in fichiers:
+    trouve_extension = False
     if ("." in fichier):
         extension = fichier.split(".")[-1].upper()
         for ext, description in definition_extensions:
             if extension == ext.upper():
-                trouve=True
+                trouve_extension=True
                 print (fichier + " (" + description + ")")
-        if not trouve:
+        if not trouve_extension:
          print(fichier + " (Extension non connue)")
 
     else :
-        print (fichier + " (Aucune extension)")
+        print (fichier + " (Aucune extension)")'''
 # Version 2 : unpacking (déballage)
 # Python "ouvre" directement le tuple pour nous
 # ext = element[0]
@@ -49,37 +49,29 @@ for fichier in fichiers:
 # => code plus lisible et plus propre
 
 
+def extraire_extension(nom_fichier):
+        if ("." in nom_fichier):
+            extension = nom_fichier.split(".")[-1].upper()
+            return extension
+        return None
+
+def get_definition_extension(extension,definitions):
+    for ext, description in definitions:
+        if extension == ext.upper():
+           return description
+    return None
+
+
+for fichier in fichiers:
+    ext = extraire_extension(fichier)
+    if ext:
+        definition = get_definition_extension(ext, definition_extensions)
+        if not definition:
+            definition = "Extension non connue"
+    else:
+        definition = "Aucune extension"
+    print(fichier + " (" + definition + ")")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-'''
-notepad.exe (Executable)
-mon.fichier.perso.doc (Document Word)
-notes.TXT (Document Texte)
-vacances.jpeg (Image JPEG)
-planning (Aucune extension)
-data.dat (Extension non connue)
-'''
-
-# lower/upper
-# in / index / for
-# split
-# -1
-
-# extraire extension
-
-
-
-# faire la correspondance définition
 
