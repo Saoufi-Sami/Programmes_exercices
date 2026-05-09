50d36ede3d9597ea78f8dc5bf5d24527413d4fc0
saoufi sami <saoufi-sami@hotmail.fr>
2026-05-09 22:18:48 +0200

    init exe count full float of caracteres

---
+++ programme_collections/exercice_count.py
@@ -0,0 +1,5 @@
+# LES COLLECTIONS
+# Exercice "Nombre total de caracteres"
+
+#         0         1        2          3         4      5
+noms = ["Jean", "Sophie","Martin","Christophe","Zoe","Martin"]
